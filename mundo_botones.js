0c93b38c027737372264a7b63d2f8ab20d5402f5
Zanahorio326 <kevinbenjamingonzalezolate@gmail.com>
2025-03-23 00:04:33 -0300

    Actualizar mundo_botones.js

--- mundo_botones.js
+++ mundo_botones.js
@@ -13,5 +13,6 @@ document.getElementById("btnHerramientas").addEventListener("click", function() 
 document.getElementById("btnChat").addEventListener("click", function() {
   const chat = document.getElementById("chat");
   chat.classList.toggle("mostrar");
-  chat.innerHTML = `<iframe src="https://zanahorio326.github.io/Mal-comienzo/chat.html" width="100%" height="100%" frameborder="0"></iframe>`;
+  // El iframe ahora tiene un alto reducido para no tocar los botones superior e inferior.
+  chat.innerHTML = `<iframe src="https://zanahorio326.github.io/Mal-comienzo/chat.html" width="100%" height="calc(100% - 60px)" frameborder="0"></iframe>`;
 });
