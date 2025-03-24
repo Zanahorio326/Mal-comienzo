c06e89d1dec8967206647e80546ea50d58a34db7
Zanahorio326 <kevinbenjamingonzalezolate@gmail.com>
2025-03-24 04:00:18 -0300

    Actualizar mundo_recursos.js

--- mundo_recursos.js
+++ mundo_recursos.js
@@ -1,14 +1,7 @@
-// mundo_recursos.js
-
-// Función que dibuja la animación de brazos y piernas del personaje al caminar.
-// Recibe como parámetro "moving" para saber si se debe animar.
-function applyWalkingAnimation(moving) {
-  // Calcula el ángulo de animación: si se mueve, se usa una función seno para generar el efecto oscilatorio.
-  let angle = moving ? sin(frameCount * 0.2) * 10 : 0;
-  // Dibujar brazos
+function animateLimbs(isMoving) {
+  let angle = isMoving ? sin(frameCount * 0.2) * 10 : 0;
   line(-10, 5, -20, 15 + angle);
   line(10, 5, 20, 15 - angle);
-  // Dibujar piernas
   line(-5, 30, -5, 50 + angle);
   line(5, 30, 5, 50 - angle);
 }
